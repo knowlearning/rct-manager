8d6b118d1f8b0e9301028efc144e0c06add3015a
Jason Ford <fordjason@gmail.com>
2025-10-07 15:47:03 -0700

    Add study admins

--- src/study-manager.vue
+++ src/study-manager.vue
@@ -6,6 +6,7 @@
   const study = reactive(await Agent.state(id))
 
   const STUDY_BUCKET_TAG = 'f0120320-a30d-11f0-82b1-eb4c2023eb1c'
+  const STUDY_ADMIN_TAG = '68be11d0-a3cf-11f0-9d1f-fb46da0b85d1'
   const STUDY_CONTEXT = [ id ]
 </script>
 
@@ -19,6 +20,11 @@
     label="Study Description"
     v-model="study.description"
     variant="outlined"
+  />
+  <TagManager
+    :tag="STUDY_ADMIN_TAG"
+    :context="STUDY_CONTEXT"
+    input-label="Add New Admin"
   />
   <TagManager
     :tag="STUDY_BUCKET_TAG"
